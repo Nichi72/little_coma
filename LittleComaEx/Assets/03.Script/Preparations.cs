385e8e22691a10b2a87a1873c3222046aa2f600c
김태수 <dhemdkdl65@naver.com>
2017-07-24 23:03:34 +0900

    2017.07.24 - 작업 완료
    오늘자 작업 완료
    - 사운드 매니저 수정
    - 버튼 매니저 수정
    - 플레이어 이동 좌표 수정
    - 플레이어 이미지 수정

---
+++ LittleComaEx/Assets/03.Script/Preparations.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Preparations : MonoBehaviour {
+
+    SoundManager soundManager;
+    public AudioClip BGM;
+
+    // Use this for initialization
+    void Start () {
+        print("BGM_Start");
+        soundManager = SoundManager._instence;
+        soundManager.playBGM(BGM);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
